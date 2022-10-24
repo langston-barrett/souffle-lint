a944460034f8eb3622f8df8146e4e99fc3be990a
Langston Barrett <langston.barrett@gmail.com>
2022-10-24 15:04:23 -0400

    doc: Init

---
+++ doc/conf.py
@@ -0,0 +1,9 @@
+# Configuration file for the Sphinx documentation builder.
+project = 'souffle-lint'
+copyright = '2022, Langston Barrett'
+author = 'Langston Barrett'
+extensions = ['sphinx_book_theme']
+templates_path = []
+exclude_patterns = ['build', 'Thumbs.db', '.DS_Store']
+html_theme = 'sphinx_book_theme'
+html_static_path = []
